a4bc4b7c8bbd96e0ed4900e4e06ab912d7341f2c
CodeLionX <sebastian.schmidl@t-online.de>
2022-08-18 09:56:33 +0200

    fix(kmeans): handle slides != 1 correctly

--- kmeans/README.md
+++ kmeans/README.md
@@ -49,18 +49,4 @@ Big `stride` (stride == 20)
 
 ## Notes
 
-KMeans outputs anomaly scores for windows.
-The results require post-processing.
-The scores for each point can be assigned by aggregating the anomaly scores for each window the point is included in.
-
-U can use the following code snippet for the post-processing step in TimeEval (default parameters directly filled in from the source code):
-
-<!--BEGIN:timeeval-post-->
-```python
-from timeeval.utils.window import ReverseWindowing
-# post-processing for k-means
-def post_kmeans(scores: np.ndarray, args: dict) -> np.ndarray:
-    window_size = args.get("hyper_params", {}).get("anomaly_window_size", 20)
-    return ReverseWindowing(window_size=window_size).fit_transform(scores)
-```
-<!--END:timeeval-post-->
+KMeans automatically computes point-wise anomaly scores.
--- kmeans/kmeans/model.py
+++ kmeans/kmeans/model.py
@@ -10,11 +10,37 @@ class KMeansAD(BaseEstimator, OutlierMixin):
         self.window_size = window_size
         self.stride = stride
         self.model = KMeans(n_clusters=k, n_jobs=n_jobs)
+        self.padding_length = 0
 
     def _preprocess_data(self, X: np.ndarray) -> np.ndarray:
         flat_shape = (X.shape[0] - (self.window_size - 1), -1)  # in case we have a multivariate TS
-        slides = sliding_window_view(X, window_shape=self.window_size, axis=0).reshape(flat_shape)
-        return slides[::self.stride, :]
+        slides = sliding_window_view(X, window_shape=self.window_size, axis=0).reshape(flat_shape)[::self.stride, :]
+        self.padding_length = X.shape[0] - (slides.shape[0] * self.stride + self.window_size - self.stride)
+        print(f"Required padding_length={self.padding_length}")
+        return slides
+
+    def _custom_reverse_windowing(self, scores: np.ndarray) -> np.ndarray:
+        print("Reversing window-based scores to point-based scores:")
+        print(f"Before reverse-windowing: scores.shape={scores.shape}")
+        # compute begin and end indices of windows
+        begins = np.array([i * self.stride for i in range(scores.shape[0])])
+        ends = begins + self.window_size
+
+        # prepare target array
+        unwindowed_length = self.stride * (scores.shape[0] - 1) + self.window_size + self.padding_length
+        mapped = np.full(unwindowed_length, fill_value=np.nan)
+
+        # only interate over window intersections
+        indices = np.unique(np.r_[begins, ends])
+        for i, j in zip(indices[:-1], indices[1:]):
+            window_indices = np.flatnonzero((begins <= i) & (j-1 < ends))
+            # print(i, j, window_indices)
+            mapped[i:j] = np.nanmean(scores[window_indices])
+
+        # replace untouched indices with 0 (especially for the padding at the end)
+        np.nan_to_num(mapped, copy=False)
+        print(f"After reverse-windowing: scores.shape={mapped.shape}")
+        return mapped
 
     def fit(self, X: np.ndarray, y=None, preprocess=True) -> 'KMeansAD':
         if preprocess:
@@ -27,7 +53,7 @@ class KMeansAD(BaseEstimator, OutlierMixin):
             X = self._preprocess_data(X)
         clusters = self.model.predict(X)
         diffs = np.linalg.norm(X - self.model.cluster_centers_[clusters], axis=1)
-        return np.repeat(diffs, self.stride)
+        return self._custom_reverse_windowing(diffs)
 
     def fit_predict(self, X, y=None) -> np.ndarray:
         X = self._preprocess_data(X)
