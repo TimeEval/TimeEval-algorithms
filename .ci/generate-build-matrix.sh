9ae03b8e5bfb24fcd6231c058ba1cabecb186f7a
CodeLionX <sebastian.schmidl@t-online.de>
2023-11-16 14:51:21 +0100

    feat: allow matrix generation script to run in different contexts (folders)

--- .ci/generate-build-matrix.sh
+++ .ci/generate-build-matrix.sh
@@ -3,8 +3,13 @@
 set -e
 
 default_branch=main
-ignore_pattern="0-base-images|1-data|2-results|3-scripts|Dockerfile|README.md|\..*|.*\.py|.*\.yml|.*\.sh|.*\.png"
+folder="${1:-.}"
+ignore_pattern="0-base-images|1-intermediate-images|2-scripts|data|results|Dockerfile|README.md|\..*|.*\.py|.*\.yml|.*\.sh|.*\.png"
 changes_in_basedir=""
+
+function echoerr () {
+  echo "$@" >&2
+}
 
 # GITHUB_EVENT_NAME=pull_request
 # GITHUB_BASE_REF=PR target branch (probably default branch)
@@ -17,32 +22,37 @@ changes_in_basedir=""
 # if this is a workflow for a PR targeting the default branch
 if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]] && [[ "$GITHUB_BASE_REF" == "$default_branch" ]]; then
   # build diff to main
-  echo "Detected pipeline for a non-default branch (assuming pull request with target $GITHUB_BASE_REF)" >&2
-  git fetch origin
-  changes_in_basedir=$( git diff --name-only "refs/remotes/origin/$GITHUB_BASE_REF..HEAD" | cut -d '/' -f 1 )
+  echoerr "Detected pipeline for a non-default branch (assuming pull request with target $GITHUB_BASE_REF)"
+  git fetch origin || echoerr "Could not update remote 'origin'! Repository might be out of date."
+  changes_in_basedir=$( git diff --name-only "refs/remotes/origin/$GITHUB_BASE_REF..HEAD" -- "$folder" | sed "s#${folder}\/##" | cut -d '/' -f 1 )
   #changes_in_basedir=$( git diff --name-only "$GITHUB_BASE_REF..HEAD" | cut -d '/' -f 1 )
 
 # if this is a workflow for the default branch
 elif [[ "$GITHUB_EVENT_NAME" == "push" ]] && [[ "$GITHUB_BASE_REF" == "$default_branch" ]]; then
   # build latest commit for the default branch
-  echo "Detected pipeline for default branch" >&2
+  echoerr "Detected pipeline for default branch"
   #changes_in_basedir=$( git diff --name-only "$CI_COMMIT_BEFORE_SHA..$CI_COMMIT_SHA" )
-  changes_in_basedir=$( git diff --name-only HEAD~1..HEAD | cut -d '/' -f 1 )
+  changes_in_basedir=$( git diff --name-only HEAD~1..HEAD -- "$folder" | sed "s#${folder}##" | cut -d '/' -f 1 )
 
-# if this is a tag-workflow: build everything
+# if this is a tag-workflow: build all algorithm images
 elif [[ "$GITHUB_EVENT_NAME" == "push" ]] && [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
-    echo "Detected pipeline for a tag" >&2
-    changes_in_basedir=$( ls -1 )
+  echoerr "Detected pipeline for a tag"
+  changes_in_basedir=$( ls -1 )
+
+else
+  echoerr "Cannot determine algorithm images to build! Please check the environment variables:"
+  env | grep "GITHUB" >&2 && true
+  echoerr ""
 fi
 
 # filter changes: remove non-algorithm-files/-folders and allow grep to find nothing (exit code 1)
 changed_algos=$( echo "$changes_in_basedir" | sort | uniq | grep -x -v -E "${ignore_pattern}" || [[ $? == 1 ]] )
-# filter changes: remove non-existing algos (e.g. when branch is not up-to-date with CI_DEFAULT_BRANCH)
+# filter changes: remove non-existing algos (e.g. when branch is not up-to-date with default branch or an algorithm was removed)
 changed_algos=$( echo "$changed_algos" | while read -r f; do [[ -d "$f" ]] && echo "$f" || true; done )
 
 if [[ -z "$changed_algos" ]]; then
-  echo "No algorithm changed!" >&2
+  echoerr "No algorithm changed!"
 fi
 
-echo "Generating pipeline for algorithms: $(xargs <<<$changed_algos)" >&2
+echoerr "Generating pipeline for algorithms: $(xargs <<<$changed_algos)"
 (jq -Rc '[.]' | jq -sc '{"algorithm_name": add}') <<<"${changed_algos}"
